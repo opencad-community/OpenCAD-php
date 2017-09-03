1a0513193e37200bde3ef4d408ef064ca74b2e6b
Phill Fernandes <phill@creativetech.me>
2017-09-02 20:58:59 -0400

    Removing oc-config.php and added oc-config.php

--- oc-config.sample.php
+++ oc-config.sample.php
@@ -4,13 +4,14 @@
 \define('COMMUNITY_NAME', 'My Community');
 
 /* Database connection variables */
-define('DB_NAME', 'opencad');
-define('DB_USER', 'root');
-define('DB_PASSWORD', '');
-define('DB_HOST', 'localhost');
+define('DB_NAME', 'YourMySQLdatabaseName');
+define('DB_USER', 'YourMySQLuser');
+define('DB_PASSWORD', 'YourMySQLDatabasePassword');
+define('DB_HOST', 'HostnameOrIPtoYourDBserver');
 
 
 /* To & From emails for system generated emails */
+/* To be used in a later version for notificaton emails */
 define('CAD_FROM_EMAIL', 'cad@community.com');
 define('CAD_FROM_NAME', COMMUNITY_NAME.' CAD');
 define('CAD_TO_EMAIL', 'admins@community.com');
