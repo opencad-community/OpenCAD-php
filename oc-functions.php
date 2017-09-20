82df3f8956277a7dadd6ffce5ace56285aa93ac4
Justin Harper <jrharper@keybase.io>
2017-09-20 16:37:33 -0400

    Introduce support for Gravatar.
    This introduces optional support for Gravatar, if a user decides to opt-out the system will fall-back to our mystery man.

--- oc-functions.php
+++ oc-functions.php
@@ -9,4 +9,23 @@ if (version_compare(PHP_VERSION, '5.5', '<' )) {
 	require_once(ABSPATH . 'vendors/password_compat/password.php');
 }
 
+/**
+ * @source https://gravatar.com/site/implement/images/php/
+ */
+function get_avatar($s = 200, $d = 'mm', $r = 'g', $img = false, $atts = array() ) {
+		if (defined( 'USE_GRAVATAR' ) && USE_GRAVATAR) {
+			$url = 'https://www.gravatar.com/avatar/';
+	    $url .= md5( strtolower( trim( $_SESSION['email'] ) ) );
+	    $url .= "?s=$s&d=$d&r=$r";
+	    if ( $img ) {
+	        $url = '<img src="' . $url . '"';
+	        foreach ( $atts as $key => $val )
+	            $url .= ' ' . $key . '="' . $val . '"';
+	        $url .= ' />';
+	    }
+	    return $url;
+		}else{
+			return "./images/user.png";
+		}
+}
 ?>
--- profile.php
+++ profile.php
@@ -67,7 +67,7 @@ include("./actions/profilActions.php");
             <!-- menu profile quick info -->
             <div class="profile clearfix">
               <div class="profile_pic">
-                <img src="./images/user.png" alt="..." class="img-circle profile_img">
+                <img src="<?php echo $get_avatar; ?>" alt="..." class="img-circle profile_img">
               </div>
               <div class="profile_info">
                 <span>Welcome,</span>
@@ -126,7 +126,7 @@ include("./actions/profilActions.php");
               <ul class="nav navbar-nav navbar-right">
                 <li class="">
                   <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-                    <img src="./images/user.png" alt=""><?php echo $name;?>
+                    <img src="<?php echo $get_avatar; ?>" alt=""><?php echo $name;?>
                     <span class=" fa fa-angle-down"></span>
                   </a>
                   <ul class="dropdown-menu dropdown-usermenu pull-right">
