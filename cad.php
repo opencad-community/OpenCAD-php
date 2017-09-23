7928e36b7189fc6157005dcbbb245a74b346eedc
Murtaza <murfasa@users.noreply.github.com>
2017-09-23 08:57:40 -0400

    Changed profile link

--- cad.php
+++ cad.php
@@ -158,7 +158,7 @@
                     <span class=" fa fa-angle-down"></span>
                   </a>
                   <ul class="dropdown-menu dropdown-usermenu pull-right">
-                    <li><a href="./profile/profile.php">My Profile</a></li>
+                    <li><a href="./profile.php">My Profile</a></li>
                     <li><a href="./actions/logout.php?responder=<?php echo $_SESSION['identifier'];?>"><i class="fa fa-sign-out pull-right"></i> Log Out</a></li>
                   </ul>
                 </li>
