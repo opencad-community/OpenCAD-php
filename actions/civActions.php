7600c255d5b8556e9d66a6c9d7b553ae9dadccfe
BuildTools <brennenstapleton@gmail.com>
2017-10-13 01:37:59 -0500

    Fixed Plate Return for Civs

--- actions/civActions.php
+++ actions/civActions.php
@@ -149,8 +149,8 @@ function ncicGetPlates()
 
             echo '
             <tr>
+                <td>'.$row[11].'</td>
                 <td>'.$row[12].'</td>
-                <td>'.$row[13].'</td>
                 <td>'.$row[2].'</td>
                 <td>'.$row[8].'</td>
                 <td>'.$row[3].'</td>
