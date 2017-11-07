20b7f34f25d72648ef029bb5b7f5671d2eb480a6
Scott Plunkett <scottplunkett15@gmail.com>
2017-11-06 23:23:17 -0600

    Fixed 500 error.
    Fixed 500 error that everyone was getting. Invalid string concatenation at line 15. Included an extra dot in the path causing oc-config.php to not properly load.

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ This program is free software: you can redistribute it and/or modify
 This program comes with ABSOLUTELY NO WARRANTY; Use at your own risk.
 **/
 
-    require_once(__DIR__ . "./oc-config.php");
+    require_once(__DIR__ . "/oc-config.php");
 
     $testing = false; //If set to true, will default some data for you
 
