eb5d2d6f223ef6cf1150ea39b9cbe41fc9ace594
Phill J. Fernandes <pfernandes@winterhillsolutions.com>
2018-08-13 22:19:25 -0400

    * OCPHP-216 Moved reset-password to modal dialog.

--- profile.php
+++ profile.php
@@ -35,6 +35,32 @@ include("./actions/profileActions.php");
         $profileUpdate = $_SESSION['profileUpdate'];
         unset($_SESSION['profileUpdate']);
     }
+
+    $passwordUpdate = "";
+      if (isset($_SESSION['passwordUpdate']))
+      {
+          $passwordUpdate = $_SESSION['passwordUpdate'];
+          unset($_SESSION['passwordUpdate']);
+      }
+  	  if (isset($_SESSION['password']))
+      {
+        $password = $_SESSION['password'];
+      }
+      $con = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+      $result = mysqli_query($con, "SELECT * FROM `users`") or die(mysqli_error($con));
+      $row = mysqli_fetch_array($result);
+      $id = $row['id'];
+      $password = $row['password'];
+      $id = $_SESSION['id'];
+      if (isset($_POST['resetpass'])) {
+      $newpassword = $_POST['password'];
+      $hashed_password = password_hash($newpassword, PASSWORD_DEFAULT);
+      mysqli_query($con,"UPDATE `users` SET `password` = '$hashed_password' WHERE `id` = '$id'") or die(mysqli_error($con));
+
+  	$_SESSION['passwordUpdate'] = '<div class="alert alert-success"><span>Your password has been updated!</span></div>';
+      }
+
+
 ?>
 
 <!DOCTYPE html>
@@ -74,7 +100,7 @@ include("./actions/profileActions.php");
                 <ul class="nav side-menu">
                   <li class="active"><a><i class="fas fa-home"></i> Home</span></a>
                     <ul class="nav child_menu" style="display: block;">
-                      <li class="current-page"><a href="javascript:void(0)">My Profile</a></li>
+                      <li class="current-page"><a href="javascript:void(0)"><i class="fas fa-user"></i> My Profile</a></li>
                     </ul>
                   </li>
                 </ul>
@@ -154,6 +180,7 @@ include("./actions/profileActions.php");
                     <div class="clearfix"></div>
                   </div>
                   <!-- ./ x_title -->
+                  <?php echo $passwordUpdate;?>
                   <div class="x_content">
                   <?php echo $profileUpdate;?>
                   <form action="<?php echo BASE_URL; ?>/actions/profileActions.php" method="post" class="form-horizontal">
@@ -179,7 +206,7 @@ include("./actions/profileActions.php");
                     <div class="form-group">
                         <label class="col-sm-2 control-label">Password:</label>
                         <div class="col-sm-10">
-                            <a class="btn btn-primary" href="<?php echo BASE_URL; ?>/reset-password.php" disab>Change Password</a>
+                            <a class="btn btn-primary" data-toggle="modal" data-target="#changePassword" ?>Change Password</a>
                         </div>
                         <!-- ./ col-sm-10 -->
                     </div>
@@ -223,15 +250,67 @@ include("./actions/profileActions.php");
       </div>
     </div>
 
+    <!-- modals -->
+
+    <!-- Change Password -->
+    <div class="modal fade" id="changePassword" tabindex="-1" role="dialog" aria-hidden="true">
+       <div class="modal-dialog modal-lg">
+          <div class="modal-content">
+             <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" id="closeChangePassword"><span aria-hidden="true">×</span>
+                </button>
+          <h4 class="modal-title" id="myModalLabel">Change Password</h4>
+        </div>
+        <!-- ./ modal-header -->
+        <div class="modal-body">
+          <form role="form" action="<?php echo BASE_URL; ?>/profile.php" method="post">
+            <div class="form-group row">
+              <label class="col-lg-2 control-label">Password</label>
+              <div class="col-lg-10">
+        <input class="form-control" type="password" name="password" id="password" size="30" maxlength="255" placeholder="Enter your new password..." value="" required <?php if ( DEMO_MODE == true ) {?> readonly <?php } ?> />
+              </div>
+              <!-- ./ col-sm-9 -->
+            </div>
+            <div class="form-group row">
+              <label class="col-lg-2 control-label">Confirm Password</label>
+              <div class="col-lg-10">
+        <input class="form-control" type="password" name="confirm_password" size="30" id="confirm_password" maxlength="255" placeholder="Retype your new password..." value="" required <?php if ( DEMO_MODE == true ) {?> readonly <?php } ?> />
+              </div>
+              <!-- ./ col-sm-9 -->
+            </div>
+        </div>
+        <!-- ./ modal-body -->
+        <div class="modal-footer">
+              <input type="submit" name="resetpass" id="resetpass" class="btn btn-primary" value="Change Password" />
+              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+          </form>
+        </div>
+        <!-- ./ modal-footer -->
+      </div>
+      <!-- ./ modal-content -->
+    </div>
+    <!-- ./ modal-dialog modal-lg -->
+  </div>
+  <!-- ./ modal fade bs-example-modal-lg -->
+
+  <!-- modals -->
+
     <?php include "./oc-includes/jquery-colsolidated.inc.php"; ?>
 
     <script>
 
     </script>
 
+    <script>
+       $(document).ready(function() {
+         getMyRank("<?php echo $_SESSION['id'];?>");
+       });
+    </script>
+
     <!-- Custom Theme Scripts -->
     <script src="./js/custom.js"></script>
     <!-- openCad Script -->
     <script src="./js/OpenCAD.js"></script>
+
   </body>
 </html>
