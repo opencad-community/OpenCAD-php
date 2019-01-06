7dfd1a0a5e4230e592d4ea104514e59210078dd9
Phill Fernandes <pfernandes@winterhillsolutions.com>
2019-01-06 16:23:25 -0500

    * OCPHP-458 Enforced PHP 7.1 as minimum required version. * OCPGP-459 Correctedt About OpenCAD - MySQL version indicator text.

--- oc-functions.php
+++ oc-functions.php
@@ -11,6 +11,13 @@ This program is free software: you can redistribute it and/or modify
 This program comes with ABSOLUTELY NO WARRANTY; Use at your own risk.
 **/
 
+if(version_compare(PHP_VERSION, '7.1', '<')) {
+	session_start();
+	$_SESSION['error_title'] = "Incompatable PHP Version";
+	$_SESSION['error'] = "An incompatable version  of PHP is active. OpenCAD requires PHP 7.1 at minimum, the current recommended version is 7.2. Currently PHP ".phpversion()." is active, please contact your server administrator.";
+	header('Location: '.BASE_URL.'/plugins/error/index.php');
+}
+
 /** Provides support for enviorments running PHP < 5.5 */
 if (version_compare(PHP_VERSION, '5.5', '<' )) {
 	require_once(ABSPATH . 'vendors/password_compat/password.php');
