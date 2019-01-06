a28fbc0530ce686829b3c2ae5420f7188fbb001c
Phill Fernandes <pfernandes@winterhillsolutions.com>
2019-01-05 20:22:46 -0500

    * OCPHP-466 Began work on application configuration absence logic. #start-progress * Added .htaccess to .gitignore

--- oc-functions.php
+++ oc-functions.php
@@ -20,6 +20,7 @@ This program comes with ABSOLUTELY NO WARRANTY; Use at your own risk.
 
  if(version_compare(PHP_VERSION, '7.1', '<')) {
 	session_start();
+	$_SESSION['error_title'] = "Incompatable PHP Version";
 	$_SESSION['error'] = "An incompatable version  of PHP is active. OpenCAD requires PHP 7.1 at minimum, the current recommended version is 7.2. Currently PHP ".phpversion()." is active, please contact your server administrator.";
 	header('Location: '.BASE_URL.'/plugins/error/index.php');
  }
@@ -42,6 +43,17 @@ if(!file_exists(getcwd().'/.htaccess') && is_writable(getcwd())){
 				."### End ATVG ErrorPages ###";
 
 	file_put_contents(getcwd().'/.htaccess', $htaccess);
+}
+
+
+if(file_exists(getcwd().'/oc-config.php') && is_writable(getcwd())){
+	if(file_exists(getcwd().'../.cpanel')){
+		session_start();
+		$_SESSION['error_title'] = "Configuration Required!";
+		$_SESSION['error'] = "OpenCAD has detected it installed on a cPanel server. Please see the install guide: https://guides.opencad.io/alldoc/installation-guides/specialized-installation-guides/install-guide-cpanel";
+		header('Location: '.BASE_URL.'/plugins/error/index.php');
+	}
+	shell_exec("cp oc-config.sample.php oc-config.php");
 }
 
 /**#@+
