ac3009d26501b473196dfdc07a57bb9650cbf89c
Phill Fernandes <pfernandes@winterhillsolutions.com>
2019-02-11 18:45:41 -0500

    * OCPHP-14 Re-adding oc-config.php check.

--- index.php
+++ index.php
@@ -12,7 +12,10 @@ This program is free software: you can redistribute it and/or modify
 This program comes with ABSOLUTELY NO WARRANTY; Use at your own risk.
 **/
 
-
+if(!file_exists(getcwd().'/oc-config.php') && is_writable(getcwd())){
+    
+   header('Location://'.$_SERVER['SERVER_NAME'].'/oc-install/start.php');
+}
 
     require_once(__DIR__ . "/oc-config.php");
 
