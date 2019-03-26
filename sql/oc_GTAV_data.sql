6201a7f57bc7e39ca5e50ce49a62c8b036fb3d72
Witherstorm <conmick@gmail.com>
2019-03-26 18:26:12 +1000

    * OCPHP-548 #comment inserted Transgender Man, Transgender Woman, Intersex, Other into gender file. #start-progress

--- sql/oc_GTAV_data.sql
+++ sql/oc_GTAV_data.sql
@@ -274,8 +274,11 @@ INSERT INTO `departments` (`department_id`, `department_name`, `department_short
 INSERT INTO `genders` (`id`, `genders`) VALUES
 (1, 'Male'),
 (2, 'Female'),
-(3, 'Transgender'),
-(4, 'Other');
+(3, 'Transgender Man'),
+(4, 'Transgender Woman'),
+(5, 'Intersex'),
+(6, 'Other');
+
 
 --
 -- Dumping data for table `incident_type`
