3332cdf9302d9931da7e0e8dc2be674b501db332
OpenCAD Translations <translate@opencad.io>
2019-04-12 17:05:23 -0400

    OCPHP-199 Merge branch 'feature/OCPHP-199_i18n' of ssh://ssh.opencad.io:7999/oc/opencad-php into feature/OCPHP-199_i18n

---
+++ oc-lang/de/de.inc.php
@@ -0,0 +1,407 @@
+<?php
+//------------------------------------------------------------------------------ 
+//*** English (en)
+//------------------------------------------------------------------------------ 
+
+$arrLang = array();
+
+$arrLang['alert_admin_email_wrong'] = "";
+$arrLang['alert_min_version_db'] = "";
+$arrLang['alert_min_version_php'] = "";
+$arrLang['alert_directory_not_writable'] = "";
+$arrLang['alert_extension_not_installed'] = "";
+$arrLang['alert_unable_to_install'] = "";
+$arrLang['alert_required_fields'] = "";
+$arrLang['alert_db_host_empty'] = "";
+$arrLang['alert_db_name_empty'] = "";
+$arrLang['alert_db_username_empty'] = "";
+$arrLang['alert_db_password_empty'] = "";
+$arrLang['alert_admin_name_empty'] = "";
+$arrLang['alert_admin_identifier_empty'] = "";
+$arrLang['alert_admin_password_empty'] = "";
+$arrLang['alert_wrong_testing_parameters'] = "";
+$arrLang['alert_remove_files'] = "";
+$arrLang['alert_wrong_parameter_passed'] = "";
+
+$arrLang['error_asp_tags'] = "";
+$arrLang['error_can_not_open_config_file'] = "";
+$arrLang['error_can_not_read_file'] = "";
+$arrLang['error_check_db_exists'] = "";
+$arrLang['error_check_db_connection'] = "";
+$arrLang['error_pdo_support'] = "";
+$arrLang['error_sql_executing'] = "";
+$arrLang['error_server_requirements'] = "";
+$arrLang['error_vd_support'] = "";
+
+$arrLang['admin_access_data'] = "";
+$arrLang['admin_access_data_descr'] = "";
+$arrLang['admin_email'] = "";
+$arrLang['admin_email_info'] = "";
+$arrLang['admin_login'] = "";
+$arrLang['admin_login_info'] = "";
+$arrLang['admin_identifier'] = "";
+$arrLang['admin_identifier_info'] = "";
+$arrLang['admin_password'] = "";
+$arrLang['admin_password_info'] = "";
+$arrLang['administrator_account'] = "";
+$arrLang['options_page'] = "";
+$arrLang['administrator_account_skipping'] = "";
+$arrLang['asp_tags'] = "";
+$arrLang['back'] = "";
+$arrLang['build_date'] = "";
+$arrLang['cancel_installation'] = "";
+$arrLang['click_start_button'] = "";
+$arrLang['click_to_start_installation'] = "";
+$arrLang['checked'] = "";
+$arrLang['complete'] = "";
+$arrLang['complete_installation'] = "";
+$arrLang['completed'] = "";
+$arrLang['continue'] = "";
+$arrLang['continue_installation'] = "";
+$arrLang['database_extension'] = "";
+$arrLang['database_host'] = "";
+$arrLang['database_host_info'] = "";
+$arrLang['database_import'] = "";
+$arrLang['database_import_error'] = "";
+$arrLang['database_name'] = "";
+$arrLang['database_name_info'] = "";
+$arrLang['database_username'] = "";
+$arrLang['database_username_info'] = "";
+$arrLang['database_password'] = "";
+$arrLang['database_password_info'] = "";
+$arrLang['database_prefix'] = "";
+$arrLang['database_prefix_info'] = "";
+$arrLang['database_settings'] = "";
+$arrLang['directories_and_files'] = "";
+$arrLang['disabled'] = "";
+$arrLang['enabled'] = "";
+$arrLang['error'] = "";
+$arrLang['extensions'] = "";
+$arrLang['getting_system_info'] = "";
+$arrLang['file_successfully_rewritten'] = "";
+$arrLang['file_successfully_deleted'] = "";
+$arrLang['file_successfully_created'] = "";
+$arrLang['failed'] = "";
+$arrLang['folder_paths'] = "";
+$arrLang['follow_the_wizard'] = "";
+$arrLang['installed'] = "";
+$arrLang['installation_complete'] = "";
+$arrLang['installation_guide'] = "";
+$arrLang['installation_type'] = "";
+$arrLang['language'] = "";
+$arrLang['license'] = "";
+$arrLang['loading'] = "";
+$arrLang['mbstring_support'] = "";
+$arrLang['magic_quotes_gpc'] = "";
+$arrLang['magic_quotes_runtime'] = "";
+$arrLang['magic_quotes_sybase'] = "";
+$arrLang['mode'] = "";
+$arrLang['modes'] = "";
+$arrLang['new_installation_of'] = "";
+$arrLang['new'] = "";
+$arrLang['no'] = "";
+$arrLang['no_writable'] = "";
+$arrLang['not_installed'] = "";
+$arrLang['off'] = "";
+$arrLang['ok'] = "";
+$arrLang['on'] = "";
+$arrLang['passed'] = "";
+$arrLang['password_encryption'] = "";
+$arrLang['perform_manual_installation'] = "";
+$arrLang['pdo_support'] = "";
+$arrLang['php_version'] = "";
+$arrLang['proceed_to_login_page'] = "";
+$arrLang['ready_to_install'] = "";
+$arrLang['remove_configuration_button'] = "";
+$arrLang['required_php_settings'] = "";
+$arrLang['safe_mode'] = "";
+$arrLang['select_installation_language'] = "";
+$arrLang['select_installation_type'] = "";
+$arrLang['sendmail_from'] = "";
+$arrLang['sendmail_path'] = "";
+$arrLang['server_api'] = "";
+$arrLang['server_requirements'] = "";
+$arrLang['session_support'] = "";
+$arrLang['short_open_tag'] = "";
+$arrLang['smtp'] = "";
+$arrLang['smtp_port'] = "";
+$arrLang['start'] = "";
+$arrLang['start_all_over'] = "";
+$arrLang['start_all_over_text'] = "";
+$arrLang['step_1_of'] = "";
+$arrLang['step_2_of'] = "";
+$arrLang['step_3_of'] = "";
+$arrLang['step_4_of'] = "";
+$arrLang['step_5_of'] = "";
+$arrLang['step_6_of'] = "";
+$arrLang['step_7_of'] = "";
+$arrLang['step_8_of'] = "";
+$arrLang['step_9_of'] = "";
+$arrLang['step_10_of'] = "";
+$arrLang['step_11_of'] = "";
+$arrLang['sub_title_message'] = "";
+$arrLang['system'] = "";
+$arrLang['system_architecture'] = "";
+$arrLang['test_connection'] = "";
+$arrLang['test_database_connection'] = "";
+$arrLang['unknown'] = "";
+$arrLang['uninstall'] = "";
+$arrLang['uninstallation_completed'] = "";
+$arrLang['update'] = "";
+$arrLang['updating_completed'] = "";
+$arrLang['virtual_directory_support'] = "";
+$arrLang['we_are_ready_to_installation'] = "";
+$arrLang['we_are_ready_to_installation_text'] = "";
+$arrLang['writable'] = "";
+
+$arrLang['core_configuration'] = "";
+$arrLang['department_configuration'] = 'Abteilungs-Konfiguration';
+$arrLang['civilian_configuration'] = ''; 
+$arrLang['administrative_configuration'] = '';
+$arrLang['extra_settings'] = '';
+
+//** Begin Core Configuration Strings **//
+$arrLang['COMMUNITY_NAME'] = "";
+$arrLang['COMMUNITY_NAME_notes'] = "";
+$arrLang['COMMUNITY_NAME_alert'] = "";
+
+$arrLang['BASE_URL'] = "";
+$arrLang['BASE_URL_notes'] = "";
+$arrLang['BASE_URL_alert'] = "";
+
+
+$arrLang['API_SECURITY'] = '';
+$arrLang['API_SECURITY_notes'] = '';
+
+$arrLang['CAD_FROM_EMAIL'] = '';
+$arrLang['CAD_FROM_EMAIL_notes'] = '';
+
+$arrLang['CAD_TO_EMAIL'] = '';
+$arrLang['CAD_TO_EMAIL_notes'] = '';
+
+$arrLang['AUTH_KEY'] = '';
+$arrLang['AUTH_SALT'] = '';
+$arrLang['SECURE_AUTH_KEY'] = '';
+$arrLang['SECURE_AUTH_SALT'] = '';
+$arrLang['LOGGED_IN_KEY'] = '';
+$arrLang['LOGGED_IN_SALT'] = '';
+$arrLang['NONCE_KEY'] = '';
+$arrLang['NONCE_SALT'] = '';
+
+//** End Core Configuration Strings **//
+
+//** Begin Police Strings **//
+$arrLang['POLICE_NCIC'] = "";
+$arrLang['POLICE_NCIC_notes'] = "";
+//** End Police Strings **//
+
+//**  Begin Fire Strings **//
+$arrLang['FIRE_PANIC'] = "";
+$arrLang['FIRE_PANIC_notes'] = "";
+
+$arrLang['FIRE_BOLO'] = "";
+$arrLang['FIRE_BOLO_notes'] = "";
+
+$arrLang['FIRE_NCIC_NAME'] = "";
+$arrLang['FIRE_NCIC_NAME_notes'] = "";
+
+$arrLang['FIRE_NCIC_PLATE'] = "";
+$arrLang['FIRE_NCIC_PLATE_notes'] = "";
+//**  End Fire Strings **//
+
+//** Begin EMS Strings **//
+$arrLang['EMS_PANIC'] = "";
+$arrLang['EMS_PANIC_notes'] = "";
+
+$arrLang['EMS_BOLO'] = "";
+$arrLang['EMS_BOLO_notes'] = "";
+
+$arrLang['EMS_NCIC_NAME'] = "";
+$arrLang['EMS_NCIC_NAME_notes'] = "";
+
+$arrLang['EMS_NCIC_PLATE'] = "";
+$arrLang['EMS_NCIC_PLATE_notes'] = "";
+//** End EMS Strings **//
+
+//** Begin Roadside Assistance Strings **//
+$arrLang['ROADSIDE_PANIC'] = "";
+$arrLang['ROADSIDE_PANIC_notes'] = "";
+
+$arrLang['ROADSIDE_BOLO'] = "";
+$arrLang['ROADSIDE_BOLO_notes'] = "";
+
+$arrLang['ROADSIDE_NCIC_NAME'] = "";
+$arrLang['ROADSIDE_NCIC_NAME_notes'] = "";
+
+$arrLang['ROADSIDE_NCIC_PLATE'] = "";
+$arrLang['ROADSIDE_NCIC_PLATE_notes'] = "";
+
+$arrLang['ROADSIDE_CALL_SELFASSIGN'] = "";
+$arrLang['ROADSIDE_cALL_SELFASSIGN`_notes'] = "";
+//** End Roadside Assistance Strings **//
+
+//** Begin Civilian Strings **//
+$arrLang['CIV_WARRANT'] = "";
+$arrLang['CIV_WARRANT_notes'] = "";
+
+$arrLang['CIV_REG'] = "";
+$arrLang['CIV_REG_notes'] = "";
+//** End Civilian Strings **//
+
+//** Begin Administrative Strings**//
+$arrLang['MODERATOR_APPROVE_USER'] = "";
+$arrLang['CIV_REG_notes'] = "";
+
+$arrLang['MODERATOR_EDIT_USER'] = "";
+$arrLang['CIV_REG_notes'] = "";
+
+$arrLang['MODERATOR_DELETE_USER'] = "";
+$arrLang['CIV_REG_notes'] = "";
+
+$arrLang['MODERATOR_SUSPEND_WITHOUT_REASON'] = "";
+$arrLang['CIV_REG_notes'] = "";
+
+$arrLang['MODERATOR_SUSPEND_WITH_REASON'] = "";
+$arrLang['CIV_REG_notes'] = "";
+
+$arrLang['MODERATOR_REACTIVATE_USER'] = "";
+$arrLang['CIV_REG_notes'] = "";
+
+$arrLang['MODERATOR_REMOVE_GROUP'] = "";
+$arrLang['CIV_REG_notes'] = "";
+
+$arrLang['MODERATOR_NCIC_EDITOR'] = "";
+$arrLang['CIV_REG_notes'] = "";
+//** End Administrative Strings**//
+
+//** Begin Extra Settings Strings **//
+$arrLang['DEMO_MODE'] = "";
+$arrLang['DEMO_MODE_notes'] = "";
+
+$arrLang['USE_GRAVATAR'] = "";
+$arrLang['USE_GRAVATAR_notes'] = "";
+//** End Extra Settings Strings **//
+
+//** Begin Common Global Strings **/
+$arrLang['WELCOME'] = "";
+$arrLang["GENERAL"] = "";
+$arrLang["CAD_SYSTEM"] = "";
+$arrLang["EDIT"] = "";
+$arrLang["DELETE"] = "";
+$arrLang["NAME"] = "";
+$arrLang["EMAIL"] = "";
+$arrLang["ROLE"] = "";
+$arrLang["IDENTIFIER"] = "";
+$arrLang["GROUPS"] = "";
+$arrLang["ACTIONS"] = "";
+$arrLang["NEXT"] = "";
+$arrLang["PREVIOUS"] = "";
+$arrLang["SEARCH"] =  "";
+$arrLang["DASHBOARD"] =  "";
+$arrLang["LOGOUT"] =  "";
+$arrLang["NEED_HELP"] =  "";
+$arrLang["FULLSCREEN"] =  "";
+$arrLang["DOB"] = "";
+$arrLang["ADDRESS"] = "";
+$arrLang["GENDER"] = "";
+$arrLang["RACE"] = "";
+$arrLang["DL_STATUS"] = "";
+$arrLang["HAIR_COLOR"] = "";
+$arrLang["BUILD"] = "";
+$arrLang["WEAPON_STATUS"] = "";
+$arrLang["WEAPON_NAME"] = "";
+$arrLang["WEAPON_TYPE"] ="";
+$arrLang["WEAPON_NOTES"] ="";
+$arrLang["DECEASED"] = "";
+$arrLang["REG_PLATE"] = "";
+$arrLang["VEHICLE_NOTES"] = "";
+$arrLang["NOT_YOU"] = "";
+$arrLang["ACTIVE_CALLS"] = "";
+$arrLang["ACTIVE_BOLOS"] = "";
+$arrLang["NCIC_NAME_LOOKUP"] = "";
+$arrLang["NCIC_PLATE_LOOKUP"] = "";
+$arrLang["NCIC_WEAPON_LOOKUP"] = "";
+$arrLang["SEND"] = "";
+$arrLang["MY_PROFILE"] = "";
+//** End Common Global Strings */
+
+//** Begin Administrator/Moderator Console Strings  **/
+$arrLang["CAD_ADMINISTRATION"] = "";
+$arrLang["STATISTICS_AT_A_GLANCE"] = "";
+$arrLang["TOTAL_USERS"] = "";
+$arrLang["ACEESS_REQUESTS"] = "";
+$arrLang["THERE_ARE_CURRENTLY_NO_ACCESS_REQUESTS"] = "";
+$arrLang["CAD_USER_MANAGEMENT"] = "";
+$arrLang["ACCOUNT_MANAGEMENT"] = "";
+$arrLang["SUSPEND_WITH_REASON"] = "";
+$arrLang["SUSPEND_WITHOUT_REASON"] = "";
+$arrLang["NCIC_EDITOR"] = "";
+$arrLang["NCIC_NAMES_DB"] = "";
+$arrLang["NCIC_NAMES_DB_none"] = "";
+$arrLang["NCIC_VEHICLES_DB"] = "";
+$arrLang["NCIC_VEHICLES_DB_none"] = "";
+$arrLang["NCIC_WEAPONS_DB"] = "";
+$arrLang["NCIC_WEAPONS_DB_none"] = "";
+$arrLang["NCIC_WARNINGS_DB"] = "";
+$arrLang["NCIC_WARNINGS_DB_none"] = "";
+$arrLang["NCIC_CITATIONS_DB"] = "";
+$arrLang["NCIC_CITATIONS_DB_none"] = "";
+$arrLang["NCIC_ARRESTS_DB"] = "";
+$arrLang["NCIC_ARRESTS_DB_none"] = "";
+$arrLang["NCIC_WARRANTS_DB"] = "";
+$arrLang["NCIC_WARRANTS_DB_none"] = "";
+//** End Administrator/Moderator Console Strings  **/
+
+//** Begin Civillian Console Strings **/
+$arrLang["CIVILLIAN_CONSOLE"] = "";
+$arrLang["MY_IDENTITIES"] = "";
+$arrLang["MY_IDENTITIES_none"] = "";
+$arrLang["MY_VEHICLES"] = "";
+$arrLang["MY_VEHICLES_none"] = "";
+$arrLang["MY_WEAPONS"] = "";
+$arrLang["MY_WEAPONS_none"] = "";
+$arrLang["MY_WARRANTS"] = "";
+$arrLang["MY_WARRANTS_none"] = "";
+$arrLang["CREATE_A_CALL"] = "";
+$arrLang["ADD_NEW_IDENTITY"] = "";
+$arrLang["Add_ADD_NEW_PLATE"] = "";
+$arrLang["ADD_NEW_WEAPON"] = "";
+$arrLang["CIVILLIAN_DASHBOARD"] = "";
+$arrLang["VIEW_WARRANTS"] = "";
+$arrLang["CREATE_WARRANT"] = "";
+$arrLang["UPDATE"] = "";
+//** End Civillian Console Strings **/
+
+//** Begin CAD Console Strings **/
+$arrLang["CAD_CONSOLE"] = "";
+$arrLang["ACTIVE_DISPATCHERS"] = "";
+$arrLang["ACTIVE_DISPATCHERS_none"] = "";
+$arrLang["AVAILABLE_UNITS"] = "";
+$arrLang["UNAVAILABLE_UNITS"] = "";
+$arrLang["NEW_PERSONS_BOLO"] = "";
+$arrLang["NEW_VEHICLE_BOLO"] = "";
+$arrLang["NEW_CALL"] = "";
+$arrLang["PANIC_BUTTON"] = "";
+$arrLang["PRIORITY_TONE"] = "";
+$arrLang["STOP_BORADCASTING_BUTTON"] = "";
+$arrLang["STOPWATCH"] = "";
+$arrLang["WARNINGS"] = "";
+$arrLang["CITATIONS"] = "";
+$arrLang["ARREST_REPORT"] = "";
+$arrLang["WARRANTS"] = "";
+//** End CAD Console Strings *//
+
+//** Begin MDT Console Strings **/
+$arrLang["MDT_CONSOLE"] = "";
+$arrLang["MY_STATUS"] = "";
+$arrLang["MY_CALL"] ="";
+$arrLang["VIEW_PERSONS_BOLOS"] = "";
+$arrLang["VIEW_VEHICLE_BOLOS"] = "";
+//** End MDT Console Strings *//
+
+//** Begin Profile Console Strings **/
+$arrLang["PASSWORD"] = "";
+$arrLang["CHANGE_PASSWORD"] = "";
+$arrLang["MY_PRFILE"] ="";
+//** End Profile Console Strings *//
+?>  
