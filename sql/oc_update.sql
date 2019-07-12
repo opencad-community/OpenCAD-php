fb1f802c998fac363e6d27e9a1a8ab97f422f26f
Phill Fernandes <pfernandes@winterhillsolutions.com>
2019-07-12 19:30:36 -0400

    * Various SQL fixes.

--- sql/oc_update.sql
+++ sql/oc_update.sql
@@ -39,53 +39,56 @@ CREATE TABLE IF NOT EXISTS `<DB_PREFIX>warrant_types` (
 ALTER TABLE `<DB_PREFIX>ncic_names` CHANGE `dl_issuer` `dl_issuer` set('Government', 'Military') COLLATE 'latin1_swedish_ci' NULL AFTER `dl_class`;
 
 
-ALTER TABLE `<DB_PREFIX>ncic_names` MODIFY IF EXISTS `dl_type`
+ALTER TABLE `<DB_PREFIX>ncic_names` CHANGE `dl_type`
 set('Not Issued','Learners','Provisional','Open','Identification Only')  DEFAULT 'Not Issued' AFTER `dl_status`;
 
-ALTER TABLE `<DB_PREFIX>ncic_names` MODIFY IF EXISTS `dl_class`
+ALTER TABLE `<DB_PREFIX>ncic_names` CHANGE `dl_class`
 set('Not Applicable', 'Car','Light Rig','Heavy Rig','Boat','Motorbike','Military') DEFAULT 'Not Applicable' AFTER `dl_type`;
 
-ALTER TABLE `<DB_PREFIX>ncic_names` MODIFY IF EXISTS `dl_issuer`
+ALTER TABLE `<DB_PREFIX>ncic_names` CHANGE `dl_issuer`
 set('Not Applicable', 'Government','Military') DEFAULT 'Not Applicable' AFTER `dl_class`;
 
-ALTER TABLE `<DB_PREFIX>ncic_names` MODIFY IF EXISTS `weapon_permit`
+ALTER TABLE `<DB_PREFIX>ncic_names` CHANGE `weapon_permit`
 set('Unobtained','Vaild','Suspended','Expired','Canceled')  AFTER `build`;
 
-ALTER TABLE `<DB_PREFIX>ncic_names` MODIFY IF EXISTS `weapon_permit_type`
+ALTER TABLE `<DB_PREFIX>ncic_names` CHANGE `weapon_permit_type`
 set('Small Arms','Specialised Weapon','Automatic Weapon','Semi-Automatic','Military Grade')  AFTER `weapon_permit`;
 
-ALTER TABLE `<DB_PREFIX>ncic_names` MODIFY IF EXISTS `weapon_permit_issuer`
+ALTER TABLE `<DB_PREFIX>ncic_names` CHANGE `weapon_permit_issuer`
 set('Ammu-Nation','Government','Military') AFTER `weapon_permit_type`;
 
-ALTER TABLE `<DB_PREFIX>ncic_names` MODIFY IF EXISTS `deceased`
+ALTER TABLE `<DB_PREFIX>ncic_names` CHANGE `deceased`
 set('NO','YES') DEFAULT 'NO' AFTER `organ_donor`;
 
-ALTER TABLE `<DB_PREFIX>ncic_names` MODIFY IF EXISTS `race`
+ALTER TABLE `<DB_PREFIX>ncic_names` CHANGE `race`
 set('Indian','Asian','Black or African American','Hispanic','Caucasian','Pacific Islander, White')  AFTER `gender`;
 
-ALTER TABLE `<DB_PREFIX>ncic_names` MODIFY IF EXISTS `blood_type`
+ALTER TABLE `<DB_PREFIX>ncic_names` CHANGE `blood_type`
 set('A+','O+','B+','AB+','A-','O-','B-','AB-') AFTER `weapon_permit_issuer`;
 
-ALTER TABLE `<DB_PREFIX>ncic_names` MODIFY IF EXISTS `organ_donor`
+ALTER TABLE `<DB_PREFIX>ncic_names` CHANGE `organ_donor`
 set('NO','YES')  DEFAULT 'NO' AFTER `blood_type`;
 
-ALTER TABLE `<DB_PREFIX>ncic_names` MODIFY IF EXISTS `build`
+ALTER TABLE `<DB_PREFIX>ncic_names` CHANGE `build`
 set('Average','Fit','Muscular','Overweight','Skinny','Thin') AFTER `hair_color`;
 
-ALTER TABLE `<DB_PREFIX>ncic_names` MODIFY IF EXISTS `hair_color`
+ALTER TABLE `<DB_PREFIX>ncic_names` CHANGE `hair_color`
 set('Bald','Black','Blonde','Blue','Brown','Gray','Green','Orange','Pink','Purple','Red','Auburn','Sandy','Strawberry','White','Partially Gray') AFTER `dl_issuer`;
 
-ALTER TABLE `<DB_PREFIX>ncic_plates` MODIFY IF EXISTS `veh_insurance`
+ALTER TABLE `<DB_PREFIX>ncic_plates` CHANGE `veh_insurance`
 set('VALID','EXPIRED','CANCELED','SUSPENDED')  AFTER `veh_scolor`;
 
-ALTER TABLE `<DB_PREFIX>ncic_plates` MODIFY IF EXISTS `veh_insurance type`
+ALTER TABLE `<DB_PREFIX>ncic_plates` CHANGE `veh_insurance type`
 set('CTP','Third Party','Comprehensive')  AFTER `veh_insurance`;
 
-ALTER TABLE `<DB_PREFIX>ncic_plates` MODIFY IF EXISTS `flags`
+ALTER TABLE `<DB_PREFIX>ncic_plates` CHANGE `flags`
 set('STOLEN','WANTED','SUSPENDED REGISTRATION','CANCELED REGISTRATION','EXPIRED REGISTRATION','INSURANCE FLAG','DRIVER FLAG','NO INSURANCE') AFTER `veh_insurance type`;
 
-ALTER TABLE `<DB_PREFIX>ncic_plates` MODIFY IF EXISTS `veh_reg_state`
+ALTER TABLE `<DB_PREFIX>ncic_plates` CHANGE `veh_reg_state`
 set('Los Santos','Blaine County','San Andreas')  AFTER `flags`;
+
+ALTER TABLE `<DB_PREFIX>ncic_weapons` CHANGE `notes` `weapon_notes` varchar(2048);
+
 
 DROP TABLE IF EXISTS
     `<DB_PREFIX>permissions`;
