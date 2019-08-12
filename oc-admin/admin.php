5e12797bdd07ea59b8d9da4bf6d11ff3bed30036
Phill Fernandes <pfernandes@winterhillsolutions.com>
2019-08-12 07:47:18 -0400

    * OCPHP-643 - Updated sidebar nav for CoreUI. * OCPHP-644 - Built base structure for pages. (OCPHP-644)

--- oc-admin/admin.php
+++ oc-admin/admin.php
@@ -76,9 +76,9 @@ This program comes with ABSOLUTELY NO WARRANTY; Use at your own risk.
 <?php include "../oc-includes/header.inc.php"; ?>
 
 
-    <body class="app header-fixed sidebar-fixed aside-menu-fixed sidebar-lg-show">
+<body class="app header-fixed sidebar-fixed aside-menu-fixed sidebar-lg-show">
 
-        <header class="app-header navbar">
+    <header class="app-header navbar">
         <button class="navbar-toggler sidebar-toggler d-lg-none mr-auto" type="button" data-toggle="sidebar-show">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -140,7 +140,7 @@ This program comes with ABSOLUTELY NO WARRANTY; Use at your own risk.
           </div>
         </li>
       </ul>
-</header>
+    </header>
 
         <div class="app-body">
         <div class="sidebar">
@@ -152,7 +152,6 @@ This program comes with ABSOLUTELY NO WARRANTY; Use at your own risk.
                 </a>
                 </li>
             </ul>
-            </nav>
                 <div class="sidebar-footer hidden-small">
                     <a data-toggle="tooltip" data-placement="top" title="Go to Dashboard"
                         href="<?php echo BASE_URL; ?>/dashboard.php">
@@ -171,14 +170,144 @@ This program comes with ABSOLUTELY NO WARRANTY; Use at your own risk.
                     </a>
                 </div>
                 <!-- /menu footer buttons -->
+                </nav>
         </div>
         <main class="main">
-            <!-- Breadcrumb-->
-            <div class="container-fluid">
-                <div class="animated fadeIn">
-                    Hello world!
+        <div class="breadcrumb" />
+        <div class="container-fluid">
+          <div class="animated fadeIn">
+            <div class="row">
+              <div class="col-sm-2 col-sm-2">
+                <div class="card text-white bg-primary">
+                  <div class="card-body pb-0">
+                    <div class="text-value">9.823</div>
+                    <div>Members online</div>
+                  </div>
                 </div>
+              </div>
+              <!-- /.col-->
+              <div class="col-sm-2 col-sm-2">
+                <div class="card text-white bg-info">
+                  <div class="card-body pb-0">
+                    <button class="btn btn-transparent p-0 float-right" type="button">
+                      <i class="icon-location-pin"></i>
+                    </button>
+                    <div class="text-value">9.823</div>
+                    <div>Members online</div>
+                  </div>
+                </div>
+              </div>
+              <!-- /.col-->
+              <div class="col-sm-2 col-sm-2">
+                <div class="card text-white bg-warning">
+                  <div class="card-body pb-0">
+                    <div class="text-value">9.823</div>
+                    <div>Members online</div>
+                  </div>
+                </div>
+              </div>
+              <!-- /.col-->
+              <div class="col-sm-2 col-sm-2">
+                <div class="card text-white bg-danger">
+                  <div class="card-body pb-0">
+                    <div class="text-value">9.82</div>
+                    <div>Members online</div>
+                  </div>
+                </div>
+              </div>
+              <!-- /.col-->
+            <div class="col-sm-2 col-sm-2">
+                <div class="card text-white bg-danger">
+                  <div class="card-body pb-0">
+                    <div class="text-value">9.82</div>
+                    <div>Members online</div>
+                  </div>
+                </div>
+              </div>
+              <!-- /.col-->
+              <div class="col-sm-2 col-sm-2">
+                <div class="card text-white bg-danger">
+                  <div class="card-body pb-0">
+                    <div class="text-value">9.82</div>
+                    <div>Members online</div>
+                  </div>
+                </div>
+              </div>
+              <!-- /.col-->
             </div>
+            <!-- /.row-->
+            <div class="card">
+              <div class="card-body">
+                <div class="row">
+                  <div class="col-sm-5">
+                    <h4 class="card-title mb-0">Traffic</h4>
+                    <div class="small text-muted">November 2017</div>
+                  </div>
+                  <!-- /.col-->
+                  <div class="col-sm-7 d-none d-md-block">
+                    <button class="btn btn-primary float-right" type="button">
+                      <i class="icon-cloud-download"></i>
+                    </button>
+                    <div class="btn-group btn-group-toggle float-right mr-3" data-toggle="buttons">
+                      <label class="btn btn-outline-secondary">
+                        <input id="option1" type="radio" name="options" autocomplete="off"> Day
+                      </label>
+                      <label class="btn btn-outline-secondary active">
+                        <input id="option2" type="radio" name="options" autocomplete="off" checked=""> Month
+                      </label>
+                      <label class="btn btn-outline-secondary">
+                        <input id="option3" type="radio" name="options" autocomplete="off"> Year
+                      </label>
+                    </div>
+                  </div>
+                  <!-- /.col-->
+                </div>
+                <!-- /.row-->
+                <div class="chart-wrapper" style="height:300px;margin-top:40px;">
+                  <canvas class="chart" id="main-chart" height="300"></canvas>
+                </div>
+              </div>
+              <div class="card-footer">
+                <div class="row text-center">
+                  <div class="col-sm-12 col-md mb-sm-2 mb-0">
+                    <div class="text-muted">Visits</div>
+                    <strong>29.703 Users (40%)</strong>
+                    <div class="progress progress-xs mt-2">
+                      <div class="progress-bar bg-success" role="progressbar" style="width: 40%" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                  </div>
+                  <div class="col-sm-12 col-md mb-sm-2 mb-0">
+                    <div class="text-muted">Unique</div>
+                    <strong>24.093 Users (20%)</strong>
+                    <div class="progress progress-xs mt-2">
+                      <div class="progress-bar bg-info" role="progressbar" style="width: 20%" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                  </div>
+                  <div class="col-sm-12 col-md mb-sm-2 mb-0">
+                    <div class="text-muted">Pageviews</div>
+                    <strong>78.706 Views (60%)</strong>
+                    <div class="progress progress-xs mt-2">
+                      <div class="progress-bar bg-warning" role="progressbar" style="width: 60%" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                  </div>
+                  <div class="col-sm-12 col-md mb-sm-2 mb-0">
+                    <div class="text-muted">New Users</div>
+                    <strong>22.123 Users (80%)</strong>
+                    <div class="progress progress-xs mt-2">
+                      <div class="progress-bar bg-danger" role="progressbar" style="width: 80%" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                  </div>
+                  <div class="col-sm-12 col-md mb-sm-2 mb-0">
+                    <div class="text-muted">Bounce Rate</div>
+                    <strong>40.15%</strong>
+                    <div class="progress progress-xs mt-2">
+                      <div class="progress-bar" role="progressbar" style="width: 40%" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- /.card-->
         </main>
 
         </div>
--- oc-admin/oc-admin-includes/sidebarNav.inc.php
+++ oc-admin/oc-admin-includes/sidebarNav.inc.php
@@ -3,7 +3,7 @@
         <li <?php if ( $pageName == "Dashboard") echo $currentPage; ?>><a href="<?php echo BASE_URL; ?>/oc-admin/admin.php"><i class="fas fa-home"></i>  Dashboard</a></li>
         <?php if ( ( MODERATOR_USER_MANAGER == true && $_SESSION['admin_privilege'] == 2 ) || ( $_SESSION['admin_privilege'] == 3 ) )
         { ?>
-        <li class="nav-item px-3" <?php if ( $pageName == "User Manager") echo $currentPage; ?>><a href="<?php echo BASE_URL; ?>/oc-admin/userManagement.php"><i class="fas fa-database fa-3px" style="color:black"></i> Users</a></li>
+        <li class="nav-item px-3" <?php if ( $pageName == "User Manager") echo $currentPage; ?>><a href="<?php echo BASE_URL; ?>/oc-admin/userManagement.php"><i class="fas fa-database fa-3px  " style="color:black"></i> Users</a></li>
       <?php  } else { }?>
         <?php if ( ( MODERATOR_NCIC_EDITOR == true && $_SESSION['admin_privilege'] == 2 ) || ( $_SESSION['admin_privilege'] == 3 ) )
         { ?>
