94e800a05275c211e8e79cc3a49f7de5979f0b8a
Phill Fernandes <pfernandes@winterhillsolutions.com>
2019-08-13 20:25:16 -0400

    * OCPHP-646 Build oc-admin/about.php.

--- oc-admin/about.php
+++ oc-admin/about.php
@@ -14,10 +14,10 @@ This program comes with ABSOLUTELY NO WARRANTY; Use at your own risk.
 **/
 
     session_start();
+
     require_once(__DIR__ . '/../oc-config.php');
     require_once(__DIR__ . '/../oc-functions.php');
-    require_once(__DIR__ . "/../oc-includes/version.php");
-    include(__DIR__ . "/../oc-includes/adminActions.php");
+    include(__DIR__."/../oc-includes/adminActions.php");
 
     if (empty($_SESSION['logged_in']))
     {
@@ -46,7 +46,7 @@ This program comes with ABSOLUTELY NO WARRANTY; Use at your own risk.
     }
     else
     {
-      permissionDenied();
+        permissionDenied();
     }
 
     $accessMessage = "";
@@ -55,216 +55,155 @@ This program comes with ABSOLUTELY NO WARRANTY; Use at your own risk.
         $accessMessage = $_SESSION['accessMessage'];
         unset($_SESSION['accessMessage']);
     }
+    $adminMessage = "";
+    if(isset($_SESSION['adminMessage']))
+    {
+        $adminMessage = $_SESSION['adminMessage'];
+        unset($_SESSION['adminMessage']);
+    }
+
+    $successMessage = "";
+    if(isset($_SESSION['successMessage']))
+    {
+        $successMessage = $_SESSION['successMessage'];
+        unset($_SESSION['successMessage']);
+    }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-
+<head>
 <?php include "../oc-includes/header.inc.php"; ?>
 
-<body class="nav-md">
-    <div class="container body">
-        <div class="main_container">
-            <div class="col-md-3 left_col">
-                <div class="left_col scroll-view">
-                    <div class="navbar nav_title" style="border: 0;">
-                        <a href="javascript:void(0)" class="site_title"><i class="fas fa-lock"></i>
-                            <span>Administrator</span></a>
-                    </div>
 
-                    <div class="clearfix"></div>
+<body class="app header-fixed">
 
-                    <!-- menu profile quick info -->
-                    <div class="profile clearfix">
-                        <div class="profile_pic">
-                            <img src="<?php echo get_avatar() ?>" alt="..." class="img-circle profile_img">
-                        </div>
-                        <div class="profile_info">
-                            <span>Welcome,</span>
-                            <h2><?php echo $name;?></h2>
-                        </div>
-                        <div class="clearfix"></div>
-                    </div>
-                    <!-- /menu profile quick info -->
+    <header class="app-header navbar">
+        <button class="navbar-toggler sidebar-toggler d-lg-none mr-auto" type="button" data-toggle="sidebar-show">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <a class="navbar-brand" href="#">
+        <img class="navbar-brand-full" src="<?php echo BASE_URL; ?>/oc-content/themes/<?php echo THEME; ?>/images/tail.png" width="30" height="25" alt="OpenCAD Logo">
+      </a>
+      <?php include "oc-admin-includes/sidebarNav.inc.php"; ?>
 
-                    <br />
+      <ul class="nav navbar-nav ml-auto">
 
-                    <?php include (__DIR__ ."/oc-admin-includes/sidebarNav.inc.php"); ?>
-
-                    <!-- /menu footer buttons -->
-                    <div class="sidebar-footer hidden-small">
-                        <a data-toggle="tooltip" data-placement="top" title="Go to Dashboard"
-                            href="<?php echo BASE_URL; ?>/dashboard.php">
-                            <span class="fas fa-clipboard-list" aria-hidden="true"></span>
-                        </a>
-                        <a data-toggle="tooltip" data-placement="top" title="FullScreen" onClick="toggleFullScreen()">
-                            <span class="glyphicon glyphicon-fullscreen" aria-hidden="true"></span>
-                        </a>
-                        <a data-toggle="tooltip" data-placement="top" title="Need Help?"
-                            href="https://guides.opencad.io/">
-                            <span class="fas fa-info-circle" aria-hidden="true"></span>
-                        </a>
-                        <a data-toggle="tooltip" data-placement="top" title="Logout"
-                            href="<?php echo BASE_URL; ?>/oc-includes/logout.php?responder=<?php echo $_SESSION['identifier'];?>">
-                            <span class="fas fa-sign-out-alt" aria-hidden="true"></span>
-                        </a>
-                    </div>
-                    <!-- /menu footer buttons -->
-                </div>
+        <li class="nav-item dropdown">
+          <a class="nav-link nav-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+            <img src="<?php echo get_avatar() ?>" alt="..." class="img-avatar">
+          </a>
+          <div class="dropdown-menu dropdown-menu-right">
+            <div class="dropdown-header text-center">
+              <strong>Account</strong>
             </div>
-
-            <!-- top navigation -->
-            <div class="top_nav">
-                <div class="nav_menu">
-                    <nav>
-                        <div class="nav toggle">
-                            <a id="menu_toggle"><i class="fas fa-bars"></i></a>
-                        </div>
-
-                        <ul class="nav navbar-nav navbar-right">
-                            <li class="">
-                                <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown"
-                                    aria-expanded="false">
-                                    <img src="<?php echo get_avatar() ?>" alt=""><?php echo $name;?>
-                                    <span class="fas fa-angle-down"></span>
-                                </a>
-                                <ul class="dropdown-menu dropdown-usermenu pull-right">
-                                    <li><a href="<?php echo BASE_URL; ?>/profile.php"><i
-                                                class="fas fa-user pull-right"></i>My Profile</a></li>
-                                    <li><a href="<?php echo BASE_URL; ?>/oc-includes/logout.php"><i
-                                                class="fas fa-sign-out-alt pull-right"></i> Log Out</a></li>
-                                </ul>
-                            </li>
-
-
-                        </ul>
-                    </nav>
-                </div>
+            <a class="dropdown-item" href="#">
+              <i class="fa fa-bell-o"></i> Updates
+              <span class="badge badge-info">42</span>
+            </a>
+            <a class="dropdown-item" href="#">
+              <i class="fa fa-envelope-o"></i> Messages
+              <span class="badge badge-success">42</span>
+            </a>
+            <a class="dropdown-item" href="#">
+              <i class="fa fa-tasks"></i> Tasks
+              <span class="badge badge-danger">42</span>
+            </a>
+            <a class="dropdown-item" href="#">
+              <i class="fa fa-comments"></i> Comments
+              <span class="badge badge-warning">42</span>
+            </a>
+            <div class="dropdown-header text-center">
+              <strong>Settings</strong>
             </div>
-            <!-- /top navigation -->
+            <a class="dropdown-item" href="#">
+              <i class="fa fa-user"></i> Profile</a>
+            <a class="dropdown-item" href="#">
+              <i class="fa fa-wrench"></i> Settings</a>
+            <a class="dropdown-item" href="#">
+              <i class="fa fa-usd"></i> Payments
+              <span class="badge badge-dark">42</span>
+            </a>
+            <a class="dropdown-item" href="#">
+              <i class="fa fa-file"></i> Projects
+              <span class="badge badge-primary">42</span>
+            </a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">
+              <i class="fa fa-shield"></i> Lock Account</a>
+            <a class="dropdown-item" href="#">
+              <i class="fa fa-lock"></i> Logout</a>
+          </div>
+        </li>
+      </ul>
+    </header>
 
-            <!-- page content -->
-            <div class="right_col" role="main">
-                <div class="">
-                    <div class="page-title">
-                        <div class="title_left">
-                            <h3>About OpenCAD</h3>
-                        </div>
-                    </div>
-
-                    <div class="clearfix"></div>
-
-                    <div class="row">
-                        <div class="col-md-12 col-sm-12 col-xs-12">
-                            <div class="x_panel">
-                                <div class="x_content">
-                                    <div style="text-align:center;">
-                                        <img src="<?php echo BASE_URL; ?>/oc-content/themes/<?php echo THEME; ?>/images/logo.png" width="239px" height="104px"
-                                            alt="The Official OpenCAD project logo, Three tails colors red, blue, and red, swoop down from top above the O in Open and finish just below the C in CAD. Stacked words, Open in a bold red font face, and CAD in a bold blue font face." />
-                                        <img src="<?php echo BASE_URL; ?>/oc-content/themes/<?php echo THEME; ?>/images/gplv3-127x51.png" height="128px"
-                                            width="251px" />
-                                    </div>
-                                    <div class="row tile_count">
-                                        <h2>About Your Environment</h2>
-                                        <div class="input-group">
-                                            PHP Version:<input type="text" class="form-control" readonly="readonly"
-                                                placeholder="<?php echo phpversion(); ?>" />
-                                            <p><em>Note:</em> The active version of PHP.</p>
-                                        </div>
-                                        <!-- ./ col-md-2 col-sm-4 col-xs-6 tile_stats_count -->
-                                        <div class="input-group">
-                                            Database Engine:<input type="text" class="form-control" readonly="readonly"
-                                                placeholder="<?php echo getMySQLVersion(); ?>" />
-                                            <p><em>Note:</em> The database engine which is currently deployed on the
-                                                server.</p>
-                                        </div>
-                                    </div>
-                                    <!-- ./ row tile_count -->
-                                    <div class="row tile_count">
-                                        <h2>About Your Application</h2>
-                                        <div class="input-group">
-                                            OpenCAD Version:<input type="text" class="form-control" readonly="readonly"
-                                                placeholder="<?php echo $oc_version ?>" />
-                                            <p><em>Note:</em> If the limit of ten (10) requests per one (1) minute the
-                                                API key will be blacklisted and support will <em>not</em> remove the
-                                                block.</p>
-                                        </div>
-                                        <div class="input-group">
-                                            OpenCAD Build:<input type="text" class="form-control" readonly="readonly"
-                                                placeholder="API KEY HERE" />
-                                            <p><em>Note:</em> If the limit of ten (10) requests per one (1) minute the
-                                                API key will be blacklisted and support will <em>not</em> remove the
-                                                block.</p>
-                                        </div>
-                                        <div class="x_content">
-                                            <div class="input-group">
-                                                API Key:
-                                                <input type="text" class="form-control" readonly="readonly"
-                                                    placeholder="<?php echo getApiKey(); ?>" />
-                                                <p>
-                                                    <em>Note:</em> Used to encrypt cookie 'aljksdz7' and authenticate
-                                                    request to the api if the requestor is not logged in.
-                                                </p>
-                                                <a style="margin-left:10px" class="btn btn-primary"
-                                                    href="<?php echo BASE_URL; ?>/oc-content/themes/<?php echo THEME; ?>/oc-includes/generalActions.php?newApiKey=1">Generate</a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <!-- ./ col-md-2 col-sm-4 col-xs-6 tile_stats_count -->
-                                </div>
-                                <!-- ./ row tile_count -->
-                                <h2>About OpenCAD</h2>
-                                <p>OpenCAD is an opensource project licensed under GNU GPL v3. The original code and
-                                    concept by <a href="https://github.com/ossified"
-                                        title="a link to the original developer's GitHub.">Shane Gill</a>. This project
-                                    is maintained Stormlight Tech.</p>
-                                <h3>Got Feedback?</h3>
-                                <p>The OpenCAD team wants to know what you think. Please send us your feedback today!
-                                </p>
-                                <a href="#" id="getFeedbackJIRA">Send Feedback</a>
-                            </div>
-                            <!-- ./ x_content -->
-                        </div>
-                        <!-- ./ x_panel -->
-                    </div>
-                    <!-- ./ col-md-12 col-sm-12 col-xs-12 -->
+      <div class="app-body">
+        <main class="main">
+        <div class="breadcrumb" />
+        <div class="container-fluid">
+          <div class="animated fadeIn">
+            <div class="card">
+            	<div class="card-header">
+          			<strong>About Your Environment</strong>
+				</div>
+              	<div class="card-body">
+					<div class="form-group">
+						<label for="name">PHP Version</label><input type="text" class="form-control" readonly="readonly" placeholder="<?php echo phpversion(); ?>" />
+						<p><em>Note:</em> The active version of PHP.</p>
+					</div>
+					<div class="form-group">
+						<label for="name">Database Engine</label>
+						<input type="text" class="form-control" readonly="readonly" placeholder="<?php echo getMySQLVersion(); ?>" />
+						<p><em>Note:</em> The database engine which is currently deployed on the server.</p>
+					</div>
+					<div class="form-group">
+						<label for="name">Loaded PHP Modules</label>
+						<input type="text" class="form-control" readonly="readonly" placeholder="<?php print_r(get_loaded_extensions()); ?>" />
+						<p><em>Note:</em> The database engine which is currently deployed on the server.</p>
+					</div>
                 </div>
-                <!-- ./ row -->
+                <!-- /.row-->
+              </div>
             </div>
-            <!-- "" -->
+            <!-- /.card-->
+
+			            <div class="card">
+            	<div class="card-header">
+          			<strong>About Your Application</strong>
+				</div>
+              	<div class="card-body">
+					<div class="form-group">
+						<label for="name">OpenCAD Version</label><input type="text" class="form-control" readonly="readonly" placeholder="<?php echo getOpenCADVersion(); ?>" />
+						<p><em>Note:</em> The active version of PHP.</p>
+					</div>
+                </div>
+                <!-- /.row-->
+              </div>
+            </div>
+            <!-- /.card-->
+        </main>
+
         </div>
-        <!-- /page content -->
+      </div>
+        <footer class="app-footer">
+        <div>
+            <a href="<?php echo BASE_URL; ?>/oc-content/themes/<?php echo THEME; ?>/images/tail.png">CoreUI Pro</a>
+            <span>&copy; 2018 creativeLabs.</span>
+        </div>
+        <div class="ml-auto">
 
-        <!-- footer content -->
-        <footer>
-            <div class="pull-right">
-                <?php echo COMMUNITY_NAME;?> CAD System
-            </div>
-            <div class="clearfix"></div>
+        </div>
+    
         </footer>
-        <!-- /footer content -->
-    </div>
-    </div>
 
-    <?php 
+    <?php
     include (__DIR__ . "/oc-admin-includes/globalModals.inc.php");
     include (__DIR__ . "/../oc-includes/jquery-colsolidated.inc.php"); ?>
-
-    <script type="text/javascript"
-        src="https://jira.opencad.io/s/a0c4d8ca8eced10a4b49aaf45ec76490-T/-f9bgig/77001/9e193173deda371ba40b4eda00f7488e/2.0.24/_/download/batch/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector.js?locale=en-US&collectorId=296607a1">
-    </script>
-
-    <script type="text/javascript">
-    window.ATL_JQ_PAGE_PROPS = {
-        "triggerFunction": function(showCollectorDialog) {
-            //Requires that jQuery is available!
-            jQuery("#getFeedbackJIRA").click(function(e) {
-                e.preventDefault();
-                showCollectorDialog();
-            });
-        }
-    };
-    </script>
 </body>
+
+            <script type="text/javascript"
+        src="https://jira.opencad.io/s/a0c4d8ca8eced10a4b49aaf45ec76490-T/-f9bgig/77001/9e193173deda371ba40b4eda00f7488e/2.0.24/_/download/batch/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector.js?locale=en-US&collectorId=ede74ac1">
+    </script>
 
 </html>
