4b42a2bd0bb2d0d22ae93b5c4df1629c73decd5f
Phill Fernandes <pfernandes@winterhillsolutions.com>
2019-08-16 12:20:36 -0400

    * Adding more session checking logic.

--- oc-admin/dataManagement/weaponManager.php
+++ oc-admin/dataManagement/weaponManager.php
@@ -13,11 +13,14 @@ This program is free software: you can redistribute it and/or modify
 This program comes with ABSOLUTELY NO WARRANTY; Use at your own risk.
 **/
 
+    if(session_id() == '' || !isset($_SESSION)) {
+    // session isn't started
     session_start();
-
-    require_once(__DIR__ . '/../oc-config.php');
-    require_once(__DIR__ . '/../oc-functions.php');
-    include(__DIR__."/../oc-includes/adminActions.php");
+    }
+    require_once('../../oc-config.php');
+    require_once( ABSPATH . '/oc-functions.php');
+    require_once( ABSPATH . "/oc-includes/adminActions.php");
+    require_once( ABSPATH . "/oc-includes/dataActions.php");
 
     if (empty($_SESSION['logged_in']))
     {
