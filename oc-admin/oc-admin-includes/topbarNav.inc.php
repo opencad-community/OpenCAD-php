c3ccb27b195a9833c707effb07202643c65c2a3f
Phill Fernandes <pfernandes@winterhillsolutions.com>
2019-08-19 08:35:54 -0400

    * OCPHP-642 Added baseline topNav for oc-admin. No logic exists, this will come in a later commit.

--- oc-admin/oc-admin-includes/topbarNav.inc.php
+++ oc-admin/oc-admin-includes/topbarNav.inc.php
@@ -14,15 +14,16 @@
         <li class="nav-item dropdown">
           <a class="nav-item dropdown-toggle" data-toggle="dropdown" href="#" role="tab" aria-haspopup="true" aria-expanded="false"><i class="fas fa-database" style="color:black"></i> <?php echo lang_key("DATA_MANAGER"); ?></a>
           <div class="dropdown-menu">
-            <a class="dropdown-item" href="<?php echo BASE_URL . "/oc-admin/dataManagement/citationTypeManager.php"; ?>"><?php echo lang_key("CITATIONTYPE_MANAGER"); ?></a>
-            <a class="dropdown-item" href="<?php echo BASE_URL . "/oc-admin/dataManagement/departmentsManager.php"; ?>"><?php echo lang_key("DEPARTMENT_MANAGER"); ?></a>
-            <a class="dropdown-item" href="<?php echo BASE_URL . "/oc-admin/dataManagement/incidentTypeManager.php"; ?>"><?php echo lang_key("INCIDENTTYPE_MANAGER"); ?></a>
-            <a class="dropdown-item" href="<?php echo BASE_URL . "/oc-admin/dataManagement/radioCodeManager.php"; ?>"><?php echo lang_key("RADIOCODE_MANAGER"); ?></a>
-            <a class="dropdown-item" href="<?php echo BASE_URL . "/oc-admin/dataManagement/streetManager.php"; ?>"><?php echo lang_key("STREET_MANAGER"); ?></a>
-            <a class="dropdown-item" href="<?php echo BASE_URL . "/oc-admin/dataManagement/vehicleManager.php"; ?>"><?php echo lang_key("VEHICLE_MANAGER"); ?></a>
-            <a class="dropdown-item" href="<?php echo BASE_URL . "/oc-admin/dataManagement/warningTypeManager.php"; ?>"><?php echo lang_key("WARNINGTYPE_MANAGER"); ?></a>
-            <a class="dropdown-item" href="<?php echo BASE_URL . "/oc-admin/dataManagement/warrantTypeManager.php"; ?>"><?php echo lang_key("WARRANTTYPE_MANAGER"); ?></a>
-            <a class="dropdown-item" href="<?php echo BASE_URL . "/oc-admin/dataManagement/weaponManager.php"; ?>"><?php echo lang_key("WEAPON_MANAGER"); ?></a>
+            <a class="dropdown-item" href="<?php echo BASE_URL . "/oc-admin/dataManagement/citationTypeManager.php"; ?>"><i class="fas fa-exclamation-triangle"></i> <?php echo lang_key("CITATIONTYPE_MANAGER"); ?></a>
+            <a class="dropdown-item" href="<?php echo BASE_URL . "/oc-admin/dataManagement/departmentsManager.php"; ?>"><i class="fas fa-exclamation-triangle"></i> <?php echo lang_key("DEPARTMENT_MANAGER"); ?></a>
+            <a class="dropdown-item" href="<?php echo BASE_URL . "/oc-admin/dataManagement/incidentTypeManager.php"; ?>"><i class="fas fa-shield-alt"></i> <?php echo lang_key("INCIDENTTYPE_MANAGER"); ?></a>
+            <a class="dropdown-item" href="<?php echo BASE_URL . "/oc-admin/dataManagement/radioCodeManager.php"; ?>"><i class="fas fa-shield-alt"></i> <?php echo lang_key("RADIOCODE_MANAGER"); ?></a>
+            <a class="dropdown-item" href="<?php echo BASE_URL . "/oc-admin/dataManagement/streetManager.php"; ?>"><i class="fas fa-road"></i> <?php echo lang_key("STREET_MANAGER"); ?></a>
+            <a class="dropdown-item" href="<?php echo BASE_URL . "/oc-admin/dataManagement/vehicleManager.php"; ?>"><i class="fas fa-motorcycle"></i> <?php echo lang_key("VEHICLE_MANAGER"); ?></a>
+            <a class="dropdown-item" href="<?php echo BASE_URL . "/oc-admin/dataManagement/warningTypeManager.php"; ?>"><i class="fas fa-exclamation-triangle"></i> <?php echo lang_key("WARNINGTYPE_MANAGER"); ?></a>
+            <a class="dropdown-item" href="<?php echo BASE_URL . "/oc-admin/dataManagement/warrantTypeManager.php"; ?>"><i class="fas fa-exclamation-triangle"></i> <?php echo lang_key("WARRANTTYPE_MANAGER"); ?></a>
+            <a class="dropdown-item" href="<?php echo BASE_URL . "/oc-admin/dataManagement/weaponManager.php"; ?>"><i class="fas fa-shield-alt"></i> <?php echo lang_key("WEAPON_MANAGER"); ?></a>
+            <a class="dropdown-item" <?php if (MODERATOR_DATAMAN_IMPEXPRES == false) echo "onclick=\"return false\""; ?> type="button" data-toggle="modal" data-target="#dataManager"> <i class="fas fa-database"></i> Import/Export/Reset</a>
           </div>
         </li>
         <?php  } else { }?>
