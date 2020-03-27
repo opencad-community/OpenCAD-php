ccd4429d4617d8b5c9e5a7c55502a30de9652ec7
Shealynn N. Stocking <sstocking@devoted.solutions>
2020-03-27 15:04:48 -0500

    providing fixes for ocphp 708 and 709

--- sql/oc_install.sql
+++ sql/oc_install.sql
@@ -71,7 +71,7 @@ CREATE TABLE IF NOT EXISTS `<DB_PREFIX>citation_types` (
   `citation_id` int(11) NOT NULL AUTO_INCREMENT,
   `citation_description` varchar(255) NOT NULL,
   `citation_fine` decimal(19,2) NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`citation_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
 
 CREATE TABLE IF NOT EXISTS `<DB_PREFIX>civilian_names` (
