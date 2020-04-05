c639e50dc64821485bb3fbb1f040eafb81a74f71
Phill J. Fernandes <pfernandes@opencad.io>
2020-04-05 18:47:04 -0400

    OCPHP-715
    * [OCPHP-715] Fixed civilian.php controls being inoperable.

--- actions/publicFunctions.php
+++ actions/publicFunctions.php
@@ -71,7 +71,7 @@ function getDataSetColumn($table, $data, $leadTrim, $followTrim)
 * @since 0.3.1
 *
 **/
-function getDataSetTable($data, $column1, $column2, $leadTrim, $followTrim)
+function getDataSetTable($data, $column1, $column2, $leadTrim, $followTrim, $isRegistration, $veh)
 {
     try{
         $pdo = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASSWORD);
@@ -96,17 +96,25 @@ function getDataSetTable($data, $column1, $column2, $leadTrim, $followTrim)
 
     foreach ($result as $row)
     {
+
+
         if( empty($row[$column2]) )
         {
             echo '
                     <option value="'. $row[$column1] . '">'. $row[$column1] .'</option>
                 ';
         }
-        if ( $isRegistration = true)
+        else if ( $isRegistration == true)
         {
             echo '
                     <option value="'. $row[$column1] . '">'. $row[$column2] .'</option>                ';
-        } else {
+        }
+        else if ( $veh == true )
+        {
+            echo '
+                    <option value="'. $row[$column1] .' ' . $row[$column2] . '">'. $row[$column1] .' '.$row[$column2] . '</option>
+                ';
+        } else{
             echo '
                     <option value="'. $row[$column1] .' | ' . $row[$column2] . '">'. $row[$column1] .' | '.$row[$column2] . '</option>
                 ';
