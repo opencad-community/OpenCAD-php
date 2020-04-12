7f6b55cf8f720bf58a06fa2185c1cd69ddb17a71
Antonio Matoš <tony@amatos.xyz>
2020-04-12 23:01:20 +0200

    Fixed an error with getDataSetTableWhere()

--- actions/publicFunctions.php
+++ actions/publicFunctions.php
@@ -82,7 +82,7 @@ function getDataSetTableWhere($dataSet, $whereString, $isTrue, $column1, $column
         die();
     }
 
-    $result = $pdo->query("SELECT * FROM ".DB_PREFIX.$dataSet." WHERE ".$whereThing." = ".$trueornotThing);
+    $result = $pdo->query("SELECT * FROM ".DB_PREFIX.$dataSet." WHERE ".$whereString." = ".$isTrue);
     if (!$result)
     {
         $_SESSION['error'] = $pdo->errorInfo();
