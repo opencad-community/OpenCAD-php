a69124fb6772d5f97cd25e21cb41633fb6a7c181
Justin Harper <jrharper@protonmail.com>
2021-04-05 23:06:51 -0400

    Drop support for end-of-life PHP and MySQL/MariaDB versions.
    Raise minimum MySQL version from 5.5 to 8.0 (8.0 recommended), MariaDB from 10.0 to 10.2 (10.5 recommended). Closes #384
    
    Raise minimum PHP version from 7.2 to 7.3 (8.0 recommend). Closes #374
    
    Minor cosmetic changes to README.md.

--- oc-includes/version.php
+++ oc-includes/version.php
@@ -13,25 +13,25 @@
  *
  * @global string $oc_version
  */
-define('OC_VERSION', '1.0');
+define('OC_VERSION', '1.0.0');
 
 /**
  * Holds the OpenCAD DB revision, increments when changes are made to the OpenCAD DB schema.
  *
  * @global int $oc_db_version
  */
-define('OC_DB_VERSION', '1.0');
+define('OC_DB_VERSION', '1.0.0');
 
 /**
  * Holds the required PHP version
  *
  * @global string $required_php_version
  */
-define('REQUIRED_PHP_VERSION', '7.2');
+define('REQUIRED_PHP_VERSION', '7.3');
 
 /**
  * Holds the required MySQL version
  *
  * @global string $required_mysql_version
  */
-define('REQUIRED_MYSQL_VERSION', '5.0');
+define('REQUIRED_MYSQL_VERSION', '8.0');
