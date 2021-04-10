e5e9f3a8132e6838bdfd33be7b49cbfdcf498fe1
Phill J, Fernandes <phillfernandes92@gmail.com>
2021-04-10 14:39:27 -0400

    * Remove reference to unused pdfmake library.

--- oc-content/themes/default/includes/scripts.inc.php
+++ oc-content/themes/default/includes/scripts.inc.php
@@ -18,8 +18,6 @@
     <script src="<?php BASE_URL . "/" . OCTHEMES . "/" . THEME; ?>/vendors/fastclick/lib/fastclick.js"></script>
 
 	<script type="text/javascript" src="<?php BASE_URL . "/" . OCTHEMES . "/" . THEME; ?>/vendors/jszip/dist/jszip.min.js"></script>
-<!--	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
-	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>-->
 	<script type="text/javascript" src="https://cdn.datatables.net/1.10.18/js/jquery.dataTables.min.js"></script>
 	<script type="text/javascript" src="https://cdn.datatables.net/1.10.18/js/dataTables.bootstrap4.min.js"></script>
 	<script type="text/javascript" src="https://cdn.datatables.net/autofill/2.3.3/js/dataTables.autoFill.min.js"></script>
