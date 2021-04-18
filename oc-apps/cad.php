30b3c1c864b03b8b9bbd0ac2dca915f9a44eaa55
Phill J, Fernandes <phillfernandes92@gmail.com>
2021-04-17 21:38:08 -0400

    * More

--- oc-apps/cad.php
+++ oc-apps/cad.php
@@ -91,7 +91,7 @@ This program comes with ABSOLUTELY NO WARRANTY; Use at your own risk.
 
 		<header class="app-header navbar">
 				<a rel="noopener" class="navbar-brand" href="#">
-					<img class="navbar-brand-full" src="<?php echo BASE_URL . "/" . OCTHEMES . "/" . THEME; ?>/images/logo_brand.png" width="30" height="25" alt="OpenCAD Logo">
+					<img class="navbar-brand-full" src="<?php echo BASE_URL . "/" . OCTHEMES . "/" . THEME; ?>/images/logo_brand.png" width="30" height="25" alt="<?php echo COMMUNITY_NAME; ?> Logo">
 				</a>
 		<?php
 			include_once ( ABSPATH . "/" . OCTHEMES . "/".THEME."/includes/topProfile.inc.php");
