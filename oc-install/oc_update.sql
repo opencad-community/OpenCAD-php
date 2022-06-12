5e6f897c00c5a54fd35167878b9ef33c7910a317
Phill j. Fernandes <pjfernandes@creativetech.me>
2022-06-12 18:23:18 -0400

    [opencad-app/opencad-php#398] Removed passwordReset column
    [opencad-app/opencad-php#398] Removed passwordReset column

--- oc-install/oc_update.sql
+++ oc-install/oc_update.sql
@@ -1,7 +1,9 @@
--- Last updated for OpenCAD 0.3.1
+-- Last updated for OpenCAD 1.0
 
 UPDATE `<DB_PREFIX>warrantTypes` SET `warrantType` = 1 WHERE `warrantType` = "Violent";
 UPDATE `<DB_PREFIX>warrantTypes` SET `warrantType` = 0 WHERE `warrantType` = "Non-Violent";
 ALTER TABLE `<DB_PREFIX>warrantTypes` ADD COLUMN `warrantViolent` int(1) NOT NULL AFTER `id`;
 
 ALTER TABLE `<DB_PREFIX>citationTypes` CHANGE COLUMN `id` `citationId` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `<DB_PREFIX>users` DROP COLUMN `passwordReset`;
